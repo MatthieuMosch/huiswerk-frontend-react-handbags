ccb54688507551119436793cefea28f502ee40cb
MatthieuMosch <mmosch@gmail.com>
2025-02-06 20:06:45 +0100

    eerste begin huiswerk

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './App.css'
 
 function App() {
   return (
-      <h1>Handbags & Purses</h1>
+      <>
+        <h1>Handbags & Purses</h1>
+        <nav>
+          <button></button>
+          <button></button>
+          <button></button>
+        </nav>
+      </>
   )
 }
 
